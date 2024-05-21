1332e0300673da769a915e053a7ca47a36171f07
nguyenquan12121 <65068099+nguyenquan12121@users.noreply.github.com>
2024-05-21 17:01:37 +0200

    Clean up `serial_motor.py`

--- serial_motor.py
+++ serial_motor.py
@@ -2,15 +2,12 @@ import serial
 import time
 
 def send_command(command, speed, duration, direction):
-   command = str(command) + "-" + str(speed) + "-" + str(duration) + "-" + str(direction) + "\n"
    delay = 3 
    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
    ser.flush()
    print(f"waiting for {delay}")
-
    time.sleep(delay)
-   ser.write(str.encode(command))
-#   ser.write(b"{command}-{speed}-{duration}-{direction}\n")
+   ser.write(str.encode("{command}-{speed}-{duration}-{direction}\n"))
    print(f"Sending command: {command}-{speed}-{duration}-{direction}")
 
    if ser.in_waiting > 0:
