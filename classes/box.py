01f731729d09413e3529ba1e59eb5ef0bdb0c81f
quan <quan177013@gmail.com>
2024-06-03 00:47:05 +0200

    added color to the box class

--- classes/box.py
+++ classes/box.py
@@ -1,15 +1,19 @@
 position_mapper = ["Conveyor Belt", "Elevator", "Pusher", "Storage"]
 
 class Box:
-    def __init__(self, box_id, location):
+    def __init__(self, box_id, location, color):
         self.box_id = box_id
         self.location = location
+        self.color = color
 
     def set_location(self, location):
         self.location = location
 
+    def set_color(self, color):
+        self.color = color
+
     def print_box(self):
-        print(f"Box ID: {self.box_id}, Location: {position_mapper[self.location]}")
+        print(f"Box ID: {self.box_id}, Location: {position_mapper[self.location]}, Color: {self.color}")
         
     def get_id(self):
         return self.box_id
