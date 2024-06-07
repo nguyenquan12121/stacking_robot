0da6acf7096038419c5a5e8c01b15dfc57154a8c
CodingDiederik <diederikwebster@ziggo.nl>
2024-06-07 17:16:36 +0200

    This works!

--- client/main.py
+++ client/main.py
@@ -6,7 +6,7 @@ def run_script(script):
 
 file1 = 'sensors.py'
 file2 = 'sendMessage.py'
-file3 = 'recieve.py'
+#file3 = 'recieve.py'
 
 
 if __name__ == '__main__':
@@ -15,14 +15,14 @@ if __name__ == '__main__':
     # Create threads
     thread1 = threading.Thread(target=run_script, args=(file1,))
     thread2 = threading.Thread(target=run_script, args=(file2,))
-    thread3 = threading.Thread(target=run_script, args=(file3,))
+    #thread3 = threading.Thread(target=run_script, args=(file3,))
 
     # Start threads
     thread1.start()
     thread2.start()
-    thread3.start()
+    #thread3.start()
 
     # Wait for both threads to complete
     thread1.join()
     thread2.join()
-    thread3.join()
+    #thread3.join()
